733ed6a08d22f3f1b120fbffc306a8bab5d74465
24LP7422 24LP7422 <24LP7422@G302-CC0115.local>
2025-05-13 07:49:08 +0800

    login comp up

---
+++ fe/src/components/logIn.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(8, { message: "Please enter at least 8 letters" })
+    .email("Please enter a valid email"),
+  password: z.string().min(4, { message: "Please enter at least 4 letters" }),
+});
+
+export const LogIn = ({ onClick }: { onClick: () => void }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("http://localhost:8000/auth", {
+        email: values.email,
+        password: values.password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error, "err");
+      if (
+        error.response &&
+        (error.response.status === 401 || error.response.status === 404)
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+    // console.log(values);
+  };
+
+  return (
+    <div>
+      <Card className="flex border-none outline-none shadow-none w-[407px] p-6 rounded-lg">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex flex-col gap-6"
+          >
+            <CardHeader>
+              <CardTitle className="text-2xl font-semibold leading-[32px] text-[#09090B]">
+                Welcome back
+              </CardTitle>
+              {/* <CardDescription className="text-sm leading-[20px] text-[#71717A]">
+                Connect email and set a password
+              </CardDescription> */}
+            </CardHeader>
+            <CardContent className="flex flex-col gap-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter email here"
+                        type="email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter password here"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button
+                onClick={onClick}
+                className="flex px-4 py-2 w-full h-[40px] items-center rounded-md opacity-[0.2] bg-[#18181B]"
+              >
+                Continue
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </div>
+  );
+};
