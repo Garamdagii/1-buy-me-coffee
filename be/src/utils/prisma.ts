af0361ee2ca7979faf4403a1042d1beff56b7b11
unknown <garamdagiii@gmail.com>
2025-05-12 18:44:28 +0800

    be db setup up

---
+++ be/src/utils/prisma.ts
@@ -0,0 +1,7 @@
+
+
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '../../generated/prisma'
+
+export const prisma = new PrismaClient().$extends(withAccelerate())
+
