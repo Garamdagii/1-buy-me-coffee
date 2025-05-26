8769eccdd578bba1f49d00df2a15929af676255a
unknown <garamdagiii@gmail.com>
2025-05-26 17:00:41 +0800

    up

--- fe/src/components/navigationButton.tsx
+++ fe/src/components/navigationButton.tsx
@@ -10,13 +10,19 @@ import {
 } from "@/components/ui/popover";
 import { ChevronDown } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export const UserInNavbar = () => {
   const { profile, setProfile } = useContext(AuthContext);
-  console.log(profile, "nav");
+
+  const router = useRouter();
+  const handleLogOut = () => {
+    router.push("/log-in");
+  };
 
   return (
-    <div className="flex justify-center items-center gap-2">
+    <div className="flex gap-2 h-40px w-full pt-[32px] pr-[80px] justify-center items-center">
       <Avatar>
         <AvatarImage src={profile?.message?.avatarImage} />
         <AvatarFallback>CN</AvatarFallback>
@@ -27,7 +33,15 @@ export const UserInNavbar = () => {
           <PopoverTrigger className="flex justify-center items-center">
             <ChevronDown />
           </PopoverTrigger>
-          <PopoverContent>Logout</PopoverContent>
+          <PopoverContent className="flex w-fit h-fit">
+            <Button
+              variant="secondary"
+              className="flex h-[40px] px-4 py-2 justify-center items-center rounded-md text-sm font-medium leading-[20px] text-[#18181B]"
+              onClick={handleLogOut}
+            >
+              Log out
+            </Button>
+          </PopoverContent>
         </Popover>
       </div>
     </div>
